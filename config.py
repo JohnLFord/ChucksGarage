189c321c9094589cf3d532aa9ce3f4ca6c3fd03a
John Ford <John Ford>
2026-08-18 16:35:10 -0500

    Configure Render deployment

--- config.py
+++ config.py
@@ -3,17 +3,25 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "super-secret-key-for-local-dev")
     RATELIMIT_STORAGE_URI = os.getenv("RATELIMIT_STORAGE_URI", "memory://")
 
+
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///testing.db"
     DEBUG = True
-    CACHE_TYPE = 'SimpleCache'
+    CACHE_TYPE = "SimpleCache"
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    CACHE_TYPE = "SimpleCache"
